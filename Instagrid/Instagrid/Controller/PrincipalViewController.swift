1310e601673a8530e0b987f82f763fa3d321f46a
yoan8306 <yoan8306@wanadoo.fr>
2021-09-03 10:08:44 +0200

    interface portrait ok

---
+++ Instagrid/Instagrid/Controller/PrincipalViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PrincipalViewController.swift
+//  Instagrid
+//
+//  Created by Yoan on 01/09/2021.
+//
+
+import UIKit
+
+class PrincipalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
