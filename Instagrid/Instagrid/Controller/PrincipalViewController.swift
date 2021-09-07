f503d0c9e2633e52db06a34ef318e21110a2ef36
yoan8306 <yoan8306@wanadoo.fr>
2021-09-07 16:53:16 +0200

    feature open library and select photo
    for each button can select photo and change photo

--- Instagrid/Instagrid/Controller/PrincipalViewController.swift
+++ Instagrid/Instagrid/Controller/PrincipalViewController.swift
@@ -9,12 +9,24 @@ import UIKit
 
 class PrincipalViewController: UIViewController {
 
+    var imagePicker = UIImagePickerController()
+
     @IBOutlet weak var photoContainer: UIView!
+
+    @IBOutlet weak var viewPhoto1: UIView!
+    @IBOutlet weak var viewPhoto2: UIView!
+    @IBOutlet weak var viewPhoto3: UIView!
+    @IBOutlet weak var viewPhoto4: UIView!
 
     @IBOutlet weak var imageButton1: UIButton!
     @IBOutlet weak var imageButton2: UIButton!
     @IBOutlet weak var imageButton3: UIButton!
     @IBOutlet weak var imageButton4: UIButton!
+
+    @IBOutlet weak var imagePhoto1: UIImageView!
+    @IBOutlet weak var imagePhoto2: UIImageView!
+    @IBOutlet weak var imagePhoto3: UIImageView!
+    @IBOutlet weak var imagePhoto4: UIImageView!
 
     @IBOutlet weak var layoutButton1: UIButton!
     @IBOutlet weak var layoutButton2: UIButton!
@@ -25,6 +37,7 @@ class PrincipalViewController: UIViewController {
     @IBOutlet weak var imageSelected3: UIImageView!
 
     var typeLayout: Layout = .layout3
+    var imageSelected: UIImageView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,9 +45,10 @@ class PrincipalViewController: UIViewController {
     }
 
     private func initializeViewContainer() {
-        let imageButtons = [imageButton1, imageButton2, imageButton3, imageButton4]
-        for button in imageButtons {
-            button?.layer.cornerRadius = 8
+        let viewButton = [viewPhoto1, imageButton1, viewPhoto2, imageButton2, viewPhoto3, imageButton3,
+                          viewPhoto4, imageButton4]
+        for view in viewButton {
+            view?.layer.cornerRadius = 8
             photoContainer.layer.cornerRadius = 10
             layoutViewCase()
         }
@@ -42,7 +56,6 @@ class PrincipalViewController: UIViewController {
 
     @IBAction func buttonLayoutAction(_ sender: UIButton) {
         switch sender {
-
         case layoutButton1:
             typeLayout = .layout1
         case layoutButton2:
@@ -55,31 +68,53 @@ class PrincipalViewController: UIViewController {
         layoutViewCase()
     }
 
+    @IBAction func selectPhoto(_ sender: UIButton) {
+        sender.backgroundColor = #colorLiteral(red: 0.06709771603, green: 0.3800081015, blue: 0.574813962, alpha: 1)
+        switch sender {
+        case imageButton1:
+            imageSelected = imagePhoto1
+        case imageButton2:
+            imageSelected = imagePhoto2
+        case imageButton3:
+            imageSelected = imagePhoto3
+        case imageButton4:
+            imageSelected = imagePhoto4
+        default:
+            break
+        }
+        openLibrary()
+    }
+
+    private func openLibrary() {
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) {
+            imagePicker.delegate = self
+            imagePicker.sourceType = .savedPhotosAlbum
+            imagePicker.allowsEditing = true
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
     private func layoutViewCase() {
-//        hideImageSelected()
-//        viewAllImageButton()
+        viewPhoto2.isHidden = typeLayout == .layout1
+        imageSelected1.isHidden = typeLayout != .layout1
 
-            imageButton2.isHidden = typeLayout == .layout1
-            imageSelected1.isHidden = typeLayout != .layout1
+        viewPhoto3.isHidden = typeLayout == .layout2
+        imageSelected2.isHidden = typeLayout != .layout2
 
-            imageButton3.isHidden = typeLayout == .layout2
-            imageSelected2.isHidden = typeLayout != .layout2
+        imageSelected3.isHidden = typeLayout != .layout3
+    }
+}
 
-            imageSelected3.isHidden = typeLayout != .layout3
-
+extension PrincipalViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
+            if let targetImage = imageSelected {
+                targetImage.image = image
+            }
+        }
+        picker.dismiss(animated: true, completion: nil)
     }
 
-    private func hideImageSelected() {
-        let allImageSelected = [imageSelected1, imageSelected2, imageSelected3]
-        for image in allImageSelected {
-            image?.isHidden = true
-        }
-    }
-
-    private func viewAllImageButton() {
-        let allImageButton = [imageButton1, imageButton2, imageButton3, imageButton4]
-        for imageButton in allImageButton {
-            imageButton?.isHidden = false
-        }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
 }
